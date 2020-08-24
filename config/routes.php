301a62320914205fc620b9f5783b249851a68cde
Rafael Risalte <risaltte@gmail.com>
2020-08-24 11:08:17 -0400

    Implentação WebServices - retornar cursos em formato Json

--- config/routes.php
+++ config/routes.php
@@ -1,24 +1,26 @@
 <?php
 
 use Alura\Cursos\Controller\{
-      Deslogar,
-      Exclusao,
-      FormularioEdicao,
-      FormularioInsercao,
-      FormularioLogin,
-      ListarCursos,
-      Persistecia,
-      RealizarLogin
+    CursosEmJson,
+    Deslogar,
+    Exclusao,
+    FormularioEdicao,
+    FormularioInsercao,
+    FormularioLogin,
+    ListarCursos,
+    Persistecia,
+    RealizarLogin
 };
 
 return $rotas = [
-      '/listar-cursos' => ListarCursos::class,
-      '/novo-curso' => FormularioInsercao::class,
-      '/salvar-curso' => Persistecia::class,
-      '/excluir-curso' => Exclusao::class,
-      '/alterar-curso' => FormularioEdicao::class,
-      '/login' => FormularioLogin::class,
-      '/realiza-login' => RealizarLogin::class,
-      '/logout' => Deslogar::class,
+    '/listar-cursos' => ListarCursos::class,
+    '/novo-curso' => FormularioInsercao::class,
+    '/salvar-curso' => Persistecia::class,
+    '/excluir-curso' => Exclusao::class,
+    '/alterar-curso' => FormularioEdicao::class,
+    '/login' => FormularioLogin::class,
+    '/realiza-login' => RealizarLogin::class,
+    '/logout' => Deslogar::class,
+    '/buscarCursosEmJson' => CursosEmJson::class,
 ];
 
